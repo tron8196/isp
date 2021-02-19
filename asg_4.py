f803b6bd361845fe1db0fda13ea2a725a9d58bfd
tron8196 <kanade850@gmail.com>
2021-02-20 00:03:19 +0530

    Added commented code for lab2 assignment

--- asg_4.py
+++ asg_4.py
@@ -2,6 +2,11 @@ import numpy as np
 import cv2
 import time
 
+'''
+Matches a given source images shape to the shape of the target image, assumption:- Target image shape > source image.
+This is done by padding zeros
+'''
+
 
 def matchImageSize(source_img, target_image_shape):
     target_img = np.zeros(target_image_shape)
@@ -41,19 +46,34 @@ def bilinearTransform(source_pixel_location):
     return np.round(newPixelIntensity, 0).astype(int)
 
 
+'''
+Returns a 2-D ndarray of n_rows*n_cols centered on top left corner.
+'''
+
+
 def returnPixelLocationArray(image_shape):
     n_rows = image_shape[0]
     n_cols = image_shape[1]
     return np.asarray([[i, j] for i in range(0, n_rows) for j in range(0, n_cols)])
 
 
-start = time.time()
+# Read the source and target image
 source_img = cv2.imread('IMG1.png', 0)
 target_img = cv2.imread('IMG2.png', 0)
+
+'''
+match the source image to target image, now all further transformations to be applied to source 
+image will be applied on this
+'''
 target_matched_source_img = matchImageSize(source_img, target_img.shape)
 
+# The two point correspondence
 source_pixel_location_list = np.array([[29, 124], [157, 372]])
 target_pixel_location_list = np.array([[93, 248], [328, 399]])
+
+'''
+Generate the 4*4 matrix as shown in the report to determine the unknowns
+'''
 
 transformation_matrix = np.array([[source_pixel_location_list[0][0], source_pixel_location_list[0][1], 1, 0],
                                   [source_pixel_location_list[0][1], -source_pixel_location_list[0][0], 0, 1],
@@ -63,8 +83,7 @@ transformation_matrix = np.array([[source_pixel_location_list[0][0], source_pixe
 target_location_column_vector = np.array([[target_pixel_location_list[0][0], target_pixel_location_list[0][1],
                                            target_pixel_location_list[1][0], target_pixel_location_list[1][1]]]).T
 
-radian_to_degree = lambda radian: (radian * 180) / np.pi
-
+# This will give a 4*1 column vector giving values for amount of translation and rotation
 variable_vector = np.matmul(np.linalg.inv(transformation_matrix), target_location_column_vector)
 
 t_x = variable_vector[2, 0]
@@ -72,43 +91,36 @@ t_y = variable_vector[3, 0]
 cos_t = variable_vector[0, 0]
 sin_t = variable_vector[1, 0]
 
-radian_rotation = np.arctan(variable_vector[1, 0] / variable_vector[0, 0])
-
-degree_rotation = np.round(radian_to_degree(radian_rotation))
-
-scaling_factor = variable_vector[1, 0] / np.sin(radian_rotation)
-
-print(scaling_factor, degree_rotation, t_x, t_y)
-
-rotation_matrix_test = np.array([[np.cos(radian_rotation), np.sin(radian_rotation)],
-                                 [-np.sin(radian_rotation), np.cos(radian_rotation)]])
-
 rotation_matrix = np.array([[cos_t, sin_t],
                             [-sin_t, cos_t]])
 
 translate_matrix = np.array([[t_x], [t_y]])
 
-target_coordinate = np.matmul(rotation_matrix, source_pixel_location_list[1].T) + translate_matrix.T
-
 n_rows = target_img.shape[0]
 n_cols = target_img.shape[1]
-
+'''
+Now we will use the determined amount of rotation and translation to transform IMG1 so that we can match
+the images taken from differing viewpoints.
+'''
 target_image = np.zeros([n_rows, n_cols])
 target_pixel_location_array = returnPixelLocationArray(target_img.shape)
 
 '''
-This piece of code, takes each pixel location from the target image and based on the geometric transform applied
-calculates the corresponding source co-ordinates, these source co-ordinates are then used to find the accurate pixel
-intensities for the location using  bilinear interpolation of the neighbourhood pixels.
+using the equation shown in the report we generate corresponding source points to get the intensity values
+to be put in the particular target location(Target to Source Mapping)
 '''
-n_rows, n_cols = target_matched_source_img.shape
 diff_target_pixel_location_array = target_pixel_location_array - translate_matrix.T
 source_pixel_location_array = np.matmul(np.linalg.inv(rotation_matrix), diff_target_pixel_location_array.T)
+
+# We use Bilinear Interpolation technique to calculate the appropriate intensity value
+
 target_image = np.apply_along_axis(bilinearTransform, 1, source_pixel_location_array.T).reshape(
     target_matched_source_img.shape)
-cv2.imwrite('temp.png', target_image)
-diff_image = np.abs(target_img - target_image)
-cv2.imwrite('temp_1.png', diff_image)
+cv2.imwrite('transformed_source.png', target_image)
 
-end = time.time()
-print(end - start)
+'''
+Now since we have manipulated IMG1 w.r.t the rotation and translation and now since the tow images correspond 
+to the same view point a simple differnce will let us know if anythig has changed in the scene
+'''
+diff_image = np.abs(target_img - target_image)
+cv2.imwrite('source_target_diff.png', diff_image)
