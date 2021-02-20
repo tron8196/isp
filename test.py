aac52ac3be2af7d2d1834fc680cee10b8f0fca94
tron8196 <kanade850@gmail.com>
2021-02-20 12:48:53 +0530

    ssh test

--- test.py
+++ test.py
@@ -1,16 +1,40 @@
-import tensorflow as tf
 import numpy as np
-from matplotlib import pyplot as plt
-x_axis = np.linspace(-5,5, 100)
-y_axis = (3*x_axis + 1)/2
 
-print(y_axis)
-plt.grid(True, which='both')
-plt.plot(x_axis, y_axis)
-plt.axhline(y=0, color='k')
-plt.axvline(x=0, color='k')
-plt.ylabel('some numbers')
-y_axis_updated = tf.math.sigmoid(y_axis)
+img_array = np.zeros([50,50])
 
-plt.plot(x_axis, y_axis_updated)
-plt.show()  
+location_index = np.random.randint(0,10, 100).reshape([50,2])
+
+
+
+
+
+
+
+
+
+# target_pixel_intensity_array = np.ones([source_pixel_location_array.shape[0], 1])
+#     pixel_row_pos = source_pixel_location_array[:, 0]
+#     pixel_col_pos = source_pixel_location_array[:, 1]
+#     bounding_row_val = np.floor(pixel_row_pos).astype(int)
+#     bounding_col_val = np.floor(pixel_col_pos).astype(int)
+#     n_rows, n_cols = source_img.shape
+#     bad_rows_to_delete_index = np.logical_or(source_pixel_location_array[:, 0]<0, source_pixel_location_array[:, 0] >= n_rows - 2)
+#     bad_cols_to_delete_index = np.logical_or(source_pixel_location_array[:, 1]<0, source_pixel_location_array[:, 1] >= n_cols - 2)
+#     data_points_to_be_deleted_index = np.logical_not(np.logical_or(bad_rows_to_delete_index, bad_cols_to_delete_index))
+#     source_pixel_location_array = source_pixel_location_array[data_points_to_be_deleted_index]
+#     target_pixel_intensity_array[data_points_to_be_deleted_index] = 0
+#
+#     pixel_row_pos = source_pixel_location_array[:, 0]
+#     pixel_col_pos = source_pixel_location_array[:, 1]
+#     bounding_row_val = np.floor(pixel_row_pos).astype(int)
+#     bounding_col_val = np.floor(pixel_col_pos).astype(int)
+#     a = pixel_row_pos - bounding_row_val
+#     b = pixel_col_pos - bounding_col_val
+#     print('----------------------------')
+#     print(np.floor(pixel_row_pos))
+#     linear_img = (1 - a) * (1 - b) * source_img[bounding_row_val, bounding_col_val] + \
+#     (1 - a) * b * source_img[bounding_row_val, bounding_col_val + 1] + \
+#     a * (1 - b) * source_img[bounding_row_val + 1, bounding_col_val] + \
+#     a * b * source_img[bounding_row_val + 1, bounding_col_val + 1]
+#
+#     return linear_img.reshape(source_img.shape)
