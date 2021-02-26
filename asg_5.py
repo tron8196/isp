dd34eb82c02d552cbd3d85ddcbaa6bc673b5da9a
tron8196 <kanade850@gmail.com>
2021-02-26 22:33:06 +0530

    homography fixed, used matlab sift code

--- asg_5.py
+++ asg_5.py
@@ -5,15 +5,10 @@ import numpy as np
 import random as rnd
 from scipy.linalg import null_space
 
-'''
-Takes images as input and outputs tuples of source and destination points with matching features
-'''
-
 
 class FourPointFeature:
     def __init__(self, random_index_list, consensus_val, test_homography_matrix):
         """
-
         :type random_index_list: list
         """
         self.test_homography_matrix = test_homography_matrix
@@ -21,43 +16,33 @@ class FourPointFeature:
         self.random_index_list = random_index_list
 
 
-def siftFeatureMatch(source_image, target_image):
-    # Initiate SIFT detector
-    sift = cv2.SIFT_create()
+'''
+Takes images as input and outputs tuples of source and destination points with matching features
+'''
 
-    # find the keypoints and descriptors with SIFT
 
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
+def bilinearTransform(source_pixel_location, source_image):
+    pixel_row_pos = source_pixel_location[0]
+    pixel_col_pos = source_pixel_location[1]
+    n_rows, n_cols = source_image.shape
+    if (not ((0 <= int(np.floor(pixel_row_pos)) < source_image.shape[0]) and
+             (0 <= int(np.floor(pixel_col_pos)) < source_image.shape[1]))):
+        return 0
+    bounding_row_val = int(np.floor(pixel_row_pos))
+    bounding_col_val = int(np.floor(pixel_col_pos))
 
-    FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-    search_params = dict(checks=50)
+    a = pixel_row_pos - bounding_row_val
+    b = pixel_col_pos - bounding_col_val
 
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    row_factor = 1 if bounding_row_val < (n_rows - 1) else -1
+    col_factor = 1 if bounding_col_val < (n_cols - 1) else -1
 
-    matches = flann.knnMatch(des1, des2, k=2)
+    newPixelIntensity = (1 - a) * (1 - b) * source_image[bounding_row_val, bounding_col_val] + \
+                        (1 - a) * b * source_image[bounding_row_val, bounding_col_val + col_factor] + \
+                        a * (1 - b) * source_image[bounding_row_val + row_factor, bounding_col_val] + \
+                        a * b * source_image[bounding_row_val + row_factor, bounding_col_val + col_factor]
+    return np.round(newPixelIntensity, 0).astype(int)
 
-    # store all the good matches as per Lowe's ratio test.
-    good = []
-    for m, n in matches:
-        if m.distance < 0.7 * n.distance:
-            good.append(m)
-
-    rc_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-
-    for point in dst_pts:
-        # print(tuple(point[0].astype(int)))
-        image = cv2.circle(img2, tuple(point[0]), 0, 0, 5)
-    cv2.imwrite('source_feature_match.png', image)
-
-    for point in rc_pts:
-        # print(tuple(point[0]))
-        image = cv2.circle(img1, tuple(point[0]), 0, 0, 5)
-    cv2.imwrite('target_feature_match.png', image)
-    # print(rc_pts.shape, dst_pts.shape)
-    return rc_pts, dst_pts
 
 
 '''
@@ -66,36 +51,26 @@ Selects random 4-point correspondences and returns the A matrix used to calculat
 
 
 def generateAMatrix(source_point_list, target_point_list):
-    # print(source_point_list.shape)
+    print(source_point_list)
+    print()
+    print(target_point_list)
     random_index_list = [rnd.randint(0, source_point_list.shape[0] - 1) for i in range(4)]
-    # print(random_index_list)
     A_matrix = np.zeros([8, 9])
 
     for index, random_index in enumerate(random_index_list):
-        # print('#######################')
-        # print('source-----point', source_point_list[random_index])
-        # print('target-----point', target_point_list[random_index])
-        # print('************************')
-        # print()
-
         A_matrix[2 * index, 0] = source_point_list[random_index, 0]
         A_matrix[2 * index, 1] = source_point_list[random_index, 1]
         A_matrix[2 * index, 2] = 1
-        A_matrix[2 * index, 6] = -source_point_list[random_index, 0] * target_point_list[
-            random_index, 0]
-        A_matrix[2 * index, 7] = -source_point_list[random_index, 1] * target_point_list[
-            random_index, 0]
+        A_matrix[2 * index, 6] = -source_point_list[random_index, 0] * target_point_list[random_index, 0]
+        A_matrix[2 * index, 7] = -source_point_list[random_index, 1] * target_point_list[random_index, 0]
         A_matrix[2 * index, 8] = -target_point_list[random_index, 0]
 
-        A_matrix[2 * index + 1, 3] = -source_point_list[random_index, 0]
-        A_matrix[2 * index + 1, 4] = -source_point_list[random_index, 1]
-        A_matrix[2 * index + 1, 5] = -1
-        A_matrix[2 * index + 1, 6] = source_point_list[random_index, 0] * \
-                                     target_point_list[random_index, 1]
-        A_matrix[2 * index + 1, 7] = source_point_list[random_index, 1] * \
-                                     target_point_list[random_index, 1]
-        A_matrix[2 * index + 1, 8] = target_point_list[random_index, 1]
-
+        A_matrix[2 * index + 1, 3] = source_point_list[random_index, 0]
+        A_matrix[2 * index + 1, 4] = source_point_list[random_index, 1]
+        A_matrix[2 * index + 1, 5] = 1
+        A_matrix[2 * index + 1, 6] = -source_point_list[random_index, 0] * target_point_list[random_index, 1]
+        A_matrix[2 * index + 1, 7] = -source_point_list[random_index, 1] * target_point_list[random_index, 1]
+        A_matrix[2 * index + 1, 8] = -target_point_list[random_index, 1]
     return A_matrix, random_index_list
 
 
@@ -107,37 +82,77 @@ def testHomography(distance_threshold, random_index_list, source_features, targe
             pass
         else:
             # source_homogeneous_point = np.ones([1,3])
-            source_homogeneous_point = np.hstack((source_target_feature_point[0], np.array([[1]])))
+            print(np.array([source_target_feature_point[0]]))
+            source_homogeneous_point = np.hstack((np.array([source_target_feature_point[0]]), np.array([[1]])))
             predicted_target_homogeneous_point = np.matmul(test_homography_matrix, source_homogeneous_point.T)
-            # print(predicted_target_homogeneous_point.T, predicted_target_homogeneous_point[1])
-            # print(predicted_target_homogeneous_point.T, source_target_feature_point[1])
             predicted_target_homogeneous_point = predicted_target_homogeneous_point / \
                                                  predicted_target_homogeneous_point[2, 0]
-            # print()
             target_predicted = np.array([predicted_target_homogeneous_point.T[0, :2]])
             target_actual = source_target_feature_point[1]
             euclid_norm = np.linalg.norm(target_actual - target_predicted)
-            # print(euclid_norm)
             if euclid_norm <= distance_threshold:
                 count = count + 1
-    # print(count/total_test_points)
     return count / total_test_points * 100 >= 80, count / total_test_points
 
 
+def siftFeatureMatchMatlab():
+    src = open('source_features.txt', 'r')
+    trg = open('target_features.txt', 'r')
+
+    src = src.readlines()
+    trg = trg.readlines()
+
+    src_features = [i.strip().split() for i in src]
+    src_features = np.array([[float(location[0]), float(location[1])] for location in src_features])
+
+    trg_features = [i.strip().split() for i in trg]
+    trg_features = np.array([[float(location[0]), float(location[1])] for location in trg_features])
+    print(src_features, trg_features)
+    return trg_features, src_features
+
+
 def getHomographyMatrix(source_image, target_image, MAX_ITERATIONS=100):
-    source_features, target_features = siftFeatureMatch(source_image, target_image)
-    four_point_corresp_dict = {}
+    source_features, target_features = siftFeatureMatchMatlab()
     iterations = 0
     while iterations <= MAX_ITERATIONS:
-        test_A_matrix, random_index_list = generateAMatrix(source_features[:, -1, :], target_features[:, -1, :])
+        test_A_matrix, random_index_list = generateAMatrix(source_features, target_features)
         test_homography_matrix = null_space(test_A_matrix)[:, 0].reshape([3, 3])
         is_valid_homography, accuracy = testHomography(10, random_index_list, source_features, target_features,
                                                        test_homography_matrix)
-        four_point_corresp_dict[iterations] = FourPointFeature(random_index_list, accuracy, test_homography_matrix)
-        iterations = iterations + 1
-    iteration_index, four_point_corresp = \
-        sorted(four_point_corresp_dict.items(), key=lambda value: value[1].consensus_val, reverse=True)[0]
-    return four_point_corresp.test_homography_matrix
+        print(is_valid_homography, accuracy)
+        if is_valid_homography:
+            break
+        MAX_ITERATIONS = MAX_ITERATIONS + 1
+    for point in list(source_features.astype(int)[np.array([random_index_list])][0]):
+        print(point)
+        image_a = cv2.circle(source_image, tuple(point), 0, 0, 5)
+        point = np.array([[point[0], point[1], 1]])
+        point_source = np.matmul(test_homography_matrix, point.T).T
+
+        point_source = point_source / np.array(point_source[:, 2])
+        print(point_source)
+
+    print()
+    cv2.imwrite('source_feature_match_random.png', image_a)
+
+    for point in list(target_features.astype(int)[np.array([random_index_list])][0]):
+        print(point)
+
+        image_b = cv2.circle(target_image, tuple(point), 0, 0, 5)
+    cv2.imwrite('target_feature_match_random.png', image_b)
+    return test_homography_matrix
+
+
+def blendValues(intensity_from_img_1, intensity_from_img_2, intensity_from_img_3):
+    if intensity_from_img_2 == 0:
+        if intensity_from_img_1 == 0:
+            return intensity_from_img_3
+        elif intensity_from_img_3 == 0:
+            return intensity_from_img_1
+        else:
+            return (intensity_from_img_1 + intensity_from_img_3) // 2
+    else:
+        return int(0.6 * intensity_from_img_2 + 0.4 * (intensity_from_img_1 + intensity_from_img_3))
 
 
 #
@@ -145,22 +160,36 @@ img1 = cv2.imread('img1.png', 0)
 img2 = cv2.imread('img2.png', 0)
 img3 = cv2.imread('img3.png', 0)
 
-
-print(img2.shape, img3.shape, img1.shape)
-
-canvas = np.zeros(([img1.shape[0], img1.shape[1]*3]))
-
-
 homography_matrix_2_1 = getHomographyMatrix(img2, img1)
-homography_matrix_2_3 = getHomographyMatrix(img2, img3)
-
-
+# homography_matrix_2_3 = getHomographyMatrix(img2, img3)
+homography_matrix_2_2 = np.identity(3)
 print(homography_matrix_2_1)
-print(homography_matrix_2_3)
-# test_homography_matrix = homography_matrix
-# random_index_list = four_point_corresp.random_index_list
-# is_valid_homography, accuracy = testHomography(10)
-#
-# print(is_valid_homography, accuracy)
-# print(homography_matrix)
-# print(four_point_corresp)
+
+canvas_rows = int(img2.shape[0] * 2)
+canvas_cols = int(img2.shape[1] * 2)
+
+canvas_img = np.zeros([canvas_rows, canvas_cols])
+canvas_pixel_location_homogeneous_array = np.hstack(
+    (canvas_img, np.ones([canvas_img.shape[0], 1])))
+
+row_offset = img2.shape[0] // 2
+col_offset = img2.shape[1] // 2
+for row_index in range(canvas_rows):
+    for col_index in range(canvas_cols):
+        current_pixel_location = np.array([[row_index - row_offset, col_index - col_offset, 1]])
+
+        img1_location = np.matmul(homography_matrix_2_1, current_pixel_location.T).T
+        img1_location = img1_location / np.array([img1_location[:, 2]]).T
+        intensity_from_img_1 = bilinearTransform(img1_location[0], img1)
+
+        img2_location = np.matmul(homography_matrix_2_2, current_pixel_location.T).T
+        img2_location = img2_location / np.array([img2_location[:, 2]]).T
+        intensity_from_img_2 = bilinearTransform(img2_location[0], img2)
+
+        # img3_location = np.matmul(homography_matrix_2_3, current_pixel_location.T).T
+        # img3_location = img3_location / np.array([img3_location[:, 2]]).T
+        # intensity_from_img_3 = bilinearTransform(img3_location[0], img3)
+        intensity_from_img_3 = 0
+        canvas_img[row_index, col_index] = blendValues(intensity_from_img_1, intensity_from_img_2, intensity_from_img_3)
+cv2.imwrite('canvas.png', canvas_img)
+# #
